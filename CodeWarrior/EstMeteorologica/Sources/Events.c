221ddc43fb29b755f1d98551a8bdf2d9d15d7d9e
Jeck Jaimes <jeckjaimes@gmail.com>
2018-04-01 16:03:18 -0400

    Códigos de processing con PWM funcionando y pruebas del Capture

---
+++ CodeWarrior/EstMeteorologica/Sources/Events.c
@@ -0,0 +1,249 @@
+/* ###################################################################
+**     Filename    : Events.c
+**     Project     : ProcessorExpert
+**     Processor   : MC9S08QE128CLK
+**     Component   : Events
+**     Version     : Driver 01.02
+**     Compiler    : CodeWarrior HCS08 C Compiler
+**     Date/Time   : 2018-03-19, 13:37, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         No public methods
+**
+** ###################################################################*/
+/*!
+** @file Events.c
+** @version 01.02
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+/* MODULE Events */
+
+
+#include "Cpu.h"
+#include "Events.h"
+
+/* User includes (#include below this line is not maintained by Processor Expert) */
+
+char countPWM = 0;
+
+/*
+** ===================================================================
+**     Event       :  AD1_OnEnd (module Events)
+**
+**     Component   :  AD1 [ADC]
+**     Description :
+**         This event is called after the measurement (which consists
+**         of <1 or more conversions>) is/are finished.
+**         The event is available only when the <Interrupt
+**         service/event> property is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void AD1_OnEnd(void)
+{
+  /* Write your code here ... */
+}
+
+
+/*
+** ===================================================================
+**     Event       :  AS1_OnError (module Events)
+**
+**     Component   :  AS1 [AsynchroSerial]
+**     Description :
+**         This event is called when a channel error (not the error
+**         returned by a given method) occurs. The errors can be read
+**         using <GetError> method.
+**         The event is available only when the <Interrupt
+**         service/event> property is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void  AS1_OnError(void)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  AS1_OnRxChar (module Events)
+**
+**     Component   :  AS1 [AsynchroSerial]
+**     Description :
+**         This event is called after a correct character is received.
+**         The event is available only when the <Interrupt
+**         service/event> property is enabled and either the <Receiver>
+**         property is enabled or the <SCI output mode> property (if
+**         supported) is set to Single-wire mode.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void  AS1_OnRxChar(void)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  AS1_OnTxChar (module Events)
+**
+**     Component   :  AS1 [AsynchroSerial]
+**     Description :
+**         This event is called after a character is transmitted.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void  AS1_OnTxChar(void)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  AS1_OnFullRxBuf (module Events)
+**
+**     Component   :  AS1 [AsynchroSerial]
+**     Description :
+**         This event is called when the input buffer is full;
+**         i.e. after reception of the last character 
+**         that was successfully placed into input buffer.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void  AS1_OnFullRxBuf(void)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  AS1_OnFreeTxBuf (module Events)
+**
+**     Component   :  AS1 [AsynchroSerial]
+**     Description :
+**         This event is called after the last character in output
+**         buffer is transmitted.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void  AS1_OnFreeTxBuf(void)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  TI1_OnInterrupt (module Events)
+**
+**     Component   :  TI1 [TimerInt]
+**     Description :
+**         When a timer interrupt occurs this event is called (only
+**         when the component is enabled - <Enable> and the events are
+**         enabled - <EnableEvent>). This event is enabled only if a
+**         <interrupt service/event> is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void TI1_OnInterrupt(void)
+{
+  /* Write your code here ... */
+		// PARA EL POTENCIOMETRO
+			if(dato==esperar){
+				dato=medir;
+			} 
+				
+			/* PARA ENVIAR UN CARACTER O UN BLOQUE POR SERIAL
+			if(dato==esperar){
+				dato=enviar;
+			} */
+	
+}
+
+/*
+** ===================================================================
+**     Event       :  TI2_OnInterrupt (module Events)
+**
+**     Component   :  TI2 [TimerInt]
+**     Description :
+**         When a timer interrupt occurs this event is called (only
+**         when the component is enabled - <Enable> and the events are
+**         enabled - <EnableEvent>). This event is enabled only if a
+**         <interrupt service/event> is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void TI2_OnInterrupt(void)
+{
+  /* Write your code here ... */
+	countPWM++;
+	
+		if(countPWM==1){
+			PWM1_Enable();
+		}
+		if(countPWM==2){
+			PWM1_Disable();
+		}
+		if(countPWM==10){
+			PWM2_Enable();
+		}
+		if(countPWM==11){
+			PWM2_Disable();
+		}
+		if(countPWM==18){
+			countPWM = 0;
+		}
+				
+}
+
+/*
+** ===================================================================
+**     Event       :  Cap1_OnCapture (module Events)
+**
+**     Component   :  Cap1 [Capture]
+**     Description :
+**         This event is called on capturing of Timer/Counter actual
+**         value (only when the component is enabled - <Enable> and the
+**         events are enabled - <EnableEvent>.This event is available
+**         only if a <interrupt service/event> is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void Cap1_OnCapture(void)
+{
+  /* Write your code here ... */
+	Cap1_GetCaptureValue(&time2);
+	Cap1_Reset();
+	
+}
+
+/* END Events */
+
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.3 [05.08]
+**     for the Freescale HCS08 series of microcontrollers.
+**
+** ###################################################################
+*/
---
+++ CodeWarrior/EstMeteorologica/Sources/ProcessorExpert.c
@@ -0,0 +1,165 @@
+/* ###################################################################
+**     Filename    : ProcessorExpert.c
+**     Project     : ProcessorExpert
+**     Processor   : MC9S08QE128CLK
+**     Version     : Driver 01.12
+**     Compiler    : CodeWarrior HCS08 C Compiler
+**     Date/Time   : 2018-03-19, 13:37, # CodeGen: 0
+**     Abstract    :
+**         Main module.
+**         This module contains user's application code.
+**     Settings    :
+**     Contents    :
+**         No public methods
+**
+** ###################################################################*/
+/*!
+** @file ProcessorExpert.c
+** @version 01.12
+** @brief
+**         Main module.
+**         This module contains user's application code.
+*/         
+/*!
+**  @addtogroup ProcessorExpert_module ProcessorExpert module documentation
+**  @{
+*/         
+/* MODULE ProcessorExpert */
+
+
+/* Including needed modules to compile this module/procedure */
+#include "Cpu.h"
+#include "Events.h"
+#include "TI1.h"
+#include "AS1.h"
+#include "AD1.h"
+#include "PWM1.h"
+#include "TI2.h"
+#include "PWM2.h"
+#include "Cap1.h"
+/* Include shared modules, which are used for whole project */
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+
+/* User includes (#include below this line is not maintained by Processor Expert) */
+/* PARA ENVIAR UN BLOQUE POR SERIAL
+
+unsigned int Enviados = 8;
+
+typedef union{
+	unsigned char u8[8];
+	unsigned int u16;
+}AMPLITUD;
+
+volatile AMPLITUD bloque; 
+
+*/
+
+
+//	PARA EL POTENCIOMETRO
+
+unsigned int envioS = 2;
+unsigned int envioP = 5;
+ 
+typedef union{
+	unsigned char u8[3];
+	unsigned int u16;
+}AMPLITUD1;
+
+volatile AMPLITUD1 ADC1, ADC2, Aux; 
+
+unsigned char env[5];
+
+void main(void)
+{
+  /* Write your local variable definition here */
+
+  /*** Processor Expert internal initialization. DON'T REMOVE THIS CODE!!! ***/
+  PE_low_level_init();
+  /*** End of Processor Expert internal initialization.                    ***/
+
+  /* Write your code here */
+  /* For example: for(;;) { } */
+  
+  dato = esperar;
+    	//  dato = enviar;                          // PARA ENVIAR UN CARACTER O UN BLOQUE POR SERIAL
+    	/* PARA ENVIAR UN BLOQUE POR SERIAL
+    	bloque.u8[0]=0x4A;
+    	bloque.u8[1]=0x65;
+    	bloque.u8[2]=0x63;
+    	bloque.u8[3]=0x6B;
+    	bloque.u8[4]=0x73;
+    	bloque.u8[5]=0x6F;  
+    	bloque.u8[6]=0x6E;
+    	bloque.u8[7]=0x20; */
+    
+    for(;;){
+  	  
+  	  //   	PARA EL POTENCIOMETRO 	  
+  	  		switch (dato){
+  	  		    		
+  	  			case esperar:
+  	  				break;
+  	  		    			
+  	  		  	case medir:
+  	  		  		
+  	  		  		//PWM1_Enable();
+  	  		  		
+  	  		  		
+  	  		  		AD1_Measure(TRUE);
+  	  		  		AD1_GetValue16(&Aux.u16);
+  	  		  		Aux.u16 = Aux.u16 >> 4;
+  	  		  		ADC1.u16 = Aux.u16 & 0x7F;
+  	  		  		ADC2.u16 = Aux.u16 >> 7;
+  	  		  		ADC2.u16 = ADC2.u16 & 0x1F;
+  	  		  		env[0] = 0xF2;
+  	  		  		env[1] = ADC2.u16;
+  	  		  		env[2] = ADC1.u16;
+  	  		  		/*env[3] = 0x00;
+  	  		  		env[4] = 0x00;*/
+  	  		  		dato = enviar;
+
+  	  		  	case enviar:
+  	  		  		//AS1_SendBlock(&Aux.u8,2,&envioS);   // Mandar valor sin protocolo
+  	  		  		AS1_SendBlock(&env,3,&envioP);    // Mandar valor con protocolo
+  	  		  		dato = esperar;
+  	  		  		  
+  	  		  	default:
+  	  		  		break;
+  	  		    	
+  	  		}  
+  	  		  
+  	  /* PARA ENVIAR UN CARACTER Y UN BLOQUE POR SERIAL
+  	  		switch (dato) {
+  	  			case enviar:
+  	  		    	//AS1_SendChar(0x42);       // PARA ENVIAR UN CARACTER POR SERIAL
+  	  		  		AS1_SendBlock(&bloque.u8,8,&Enviados);  // PARA ENVIAR UN BLOQUE POR SERIAL
+  	  		  		dato=esperar;
+  	  		  		  
+  	  		case esperar:
+  	  		  	break;
+
+  	  		}*/
+  	  
+    }
+
+  /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
+  /*** Processor Expert end of main routine. DON'T MODIFY THIS CODE!!! ***/
+  for(;;){}
+  /*** Processor Expert end of main routine. DON'T WRITE CODE BELOW!!! ***/
+} /*** End of main routine. DO NOT MODIFY THIS TEXT!!! ***/
+
+/* END ProcessorExpert */
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.3 [05.08]
+**     for the Freescale HCS08 series of microcontrollers.
+**
+** ###################################################################
+*/
